3539cad5f9d82b5da0120f4146d463be1873a68e
Claudio Stella <claudio.stella.ict@gmail.com>
2017-03-10 16:11:00 +0100

    add argument,option and flag subgenerator in place of prompts

---
+++ generators/argument/index.js
@@ -0,0 +1,49 @@
+const
+  Generator = require('yeoman-generator'),
+  chalk = require('chalk'),
+  utils = require('../utils'),
+  promptNames = require('../promptNames');
+
+module.exports = class extends Generator {
+  initializing(scriptName, values) {
+    this.argument('scriptName', {type: String, required: true});
+    if (!values) {
+      values = this.config.get(scriptName);
+      if (!values) {
+        throw new Error(`No configuration found for script: ${scriptName}`);
+      }
+    }
+    this.values = values
+  }
+
+  prompting(scriptName, values) {
+    this.log(chalk.yellow('ARGUMENTS'));
+    const that = this;
+    const done = this.async();
+    values = values || this.values;
+
+    function promptArgument(hasAnotherArgument) {
+      if (hasAnotherArgument) {
+        that.prompt(promptNames.argPrompts)
+          .then(function (argProps) {
+            values.args.push(utils.createArgument(argProps));
+            promptArgument(argProps.hasAnotherArgument);
+          });
+      } else {
+        done();
+      }
+    }
+
+    this.prompt(promptNames.args)
+      .then(function (props) {
+        promptArgument(props.hasArguments);
+      }.bind(this));
+  }
+
+  configuring(scriptName, values) {
+    if (!values) {
+      this.config.set(scriptName, this.values);
+      this.config.save();
+    }
+  }
+};
---
+++ generators/flag/index.js
@@ -0,0 +1,49 @@
+const
+  Generator = require('yeoman-generator'),
+  chalk = require('chalk'),
+  utils = require('../utils'),
+  promptNames = require('../promptNames');
+
+module.exports = class extends Generator {
+  initializing(scriptName, values) {
+    this.argument('scriptName', {type: String, required: true});
+    if (!values) {
+      values = this.config.get(scriptName);
+      if (!values) {
+        throw new Error(`No configuration found for script: ${scriptName}`);
+      }
+    }
+    this.values = values
+  }
+
+  prompting(scriptName, values) {
+    this.log(chalk.yellow('FLAGS'));
+    const that = this;
+    const done = that.async();
+    values = values || this.values;
+
+    function promptFlag(hasAnotherFlag) {
+      if (hasAnotherFlag) {
+        that.prompt(promptNames.flagPrompts)
+          .then(function (flagProps) {
+            values.flags.push(utils.createFlag(flagProps));
+            promptFlag(flagProps.hasAnotherFlag);
+          });
+      } else {
+        done();
+      }
+    }
+
+    this.prompt(promptNames.flags)
+      .then(function (props) {
+        promptFlag(props.hasFlags);
+      }.bind(this));
+  }
+
+  configuring(scriptName, values) {
+    if (!values) {
+      this.config.set(scriptName, this.values);
+      this.config.save();
+    }
+  }
+};
