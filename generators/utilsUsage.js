e9ec6d694ade715a1264765162d8eac4e8e2efef
Claudio Stella <claudio.stella.ict@gmail.com>
2017-04-02 01:18:38 +0200

    add utilsUsage to format descriptions of options

---
+++ generators/utilsUsage.js
@@ -0,0 +1,52 @@
+function createLeft(opt) {
+  function notEmpty(string) {
+    return string && string.length;
+  }
+
+  const optShort = opt.varShort, optLong = opt.varLong;
+
+  if (notEmpty(optShort) && notEmpty(optLong)) {
+    return '-' + optShort + ', --' + optLong;
+  }
+  if (notEmpty(optShort) && !notEmpty(optLong)) {
+    return '-' + optShort;
+  }
+  if (!notEmpty(optShort) && notEmpty(optLong)) {
+    return '--' + optLong;
+  }
+}
+
+function pad(varUsageLeft, max) {
+  let n = max - varUsageLeft.length;
+  let space = '  ';
+  while (n > 0) {
+    space+=' ';
+    n = n - 1;
+  }
+  return varUsageLeft + space;
+}
+
+function formatUsageDesc(options) {
+  const varUsageList = [];
+  options.forEach((option) => {
+    varUsageList.push(createLeft(option));
+  });
+
+  let max = 0;
+  varUsageList.forEach((varUsageLeft) => {
+    const len = varUsageLeft.length;
+    if (len > max) {
+      max = len;
+    }
+  });
+
+  varUsageList.forEach((varUsageLeft, i) => {
+    options[i].varUsage = pad(varUsageLeft, max) + options[i].varDesc;
+  });
+
+  return options;
+}
+
+module.exports = {
+  formatUsageDesc: formatUsageDesc,
+};
