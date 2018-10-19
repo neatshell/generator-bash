57800b620301a83a62caa37bad37b75af611452f
Claudio Stella <claudio.stella.ict@gmail.com>
2018-10-19 10:41:26 +0200

    Enable the bash:write generator to rewrite all local scripts if called without argument

--- generators/write/index.js
+++ generators/write/index.js
@@ -2,47 +2,74 @@ const
   Generator = require('yeoman-generator'),
   utils = require('../utils'),
   utilsUsage = require('../utilsUsage'),
-  promptNames = require('../promptNames'),
   fs = require('fs'),
   ejs = require('ejs');
 
 module.exports = class extends Generator {
   initializing(scriptName) {
-    this.argument('scriptName', {type: String, required: true});
     const values = this.config.get(scriptName);
     if (!values) {
-      throw new Error(`No configuration found for script: ${scriptName}`);
+      if (scriptName) {
+        throw new Error(`No configuration found for script: ${scriptName}`);
+      } else {
+        const yoRc = require(this.destinationPath('.yo-rc.json'));
+        const scriptNames = Object.keys(yoRc['generator-bash']);
+        this.scripts = [];
+        scriptNames.forEach((scriptName) => {
+          this.scripts.push({
+            name: scriptName,
+            values: this.config.get(scriptName)
+          })
+        })
+      }
+    } else {
+      this.scripts = [{
+        name: scriptName,
+        values: values
+      }];
     }
-    this.values = values;
   }
 
   reading(scriptName) {
-    const SOURCE_DIR = utils.getDir(this, scriptName);
-    const templates = this.values.templates;
-    const snippets = this.values.snippets;
+    const newScripts = [];
+    this.scripts.forEach((script) => {
+      const scriptName = script.name;
+      const values = script.values;
 
-    let compiled = [];
+      const SOURCE_DIR = utils.getDir(this, scriptName);
+      const templates = values.templates;
+      const snippets = values.snippets;
+      const compiled = [];
 
-    snippets.forEach((snippet) => {
-      let file = this.fs.read(SOURCE_DIR + snippet);
-      const isTemplate = templates.indexOf(snippet) >= 0;
-      this.values.flags = utilsUsage.formatUsageDesc(this.values.flags);
-      this.values.options = utilsUsage.formatUsageDesc(this.values.options);
+      snippets.forEach((snippet) => {
+        let file = this.fs.read(SOURCE_DIR + snippet);
+        const isTemplate = templates.indexOf(snippet) >= 0;
+        values.flags = utilsUsage.formatUsageDesc(values.flags);
+        values.options = utilsUsage.formatUsageDesc(values.options);
 
-      if (isTemplate) {
-        compiled.push(ejs.render(file, this.values));
-      } else {
-        compiled.push(file);
-      }
+        if (isTemplate) {
+          compiled.push(ejs.render(file, values));
+        } else {
+          compiled.push(file);
+        }
+      });
+      newScripts.push({
+        name: scriptName,
+        compiled: compiled
+      })
     });
-    this.compiled = compiled;
+    this.scripts = newScripts;
   }
 
   writing(scriptName) {
-    this.fs.write(scriptName, this.compiled.join('\n'));
+    this.scripts.forEach((script) => {
+      this.fs.write(script.name, script.compiled.join('\n'));
+    })
   }
 
   end(scriptName) {
-    fs.chmodSync(scriptName, '755');
+    if (!this.scripts) {
+      fs.chmodSync(scriptName, '755');
+    }
   }
 };
