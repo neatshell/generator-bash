90d61a5f952ced4a4ebda4139b426b4764cc5707
Claudio Stella <claudio.stella.ict@gmail.com>
2019-11-03 18:15:13 +0100

    Let init, just instantiate the values object the first time we run the script

--- generators/init/index.js
+++ generators/init/index.js
@@ -1,54 +1,9 @@
-const
-  Generator = require('yeoman-generator'),
-  utils = require('../utils'),
-  promptNames = require('../promptNames'),
-  hiddenSnippets = require('../snippets').hidden,
-  fs = require('fs');
+const Generator = require('yeoman-generator');
+const utils = require('../utils');
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
-    this.argument('scriptName', {
-      required: true,
-      type: String
-    });
-    this.option('silent', {
-      alias: 's',
-      default: false,
-      description: 'Whether to enable silent mode for getopts',
-      hide: false,
-      type: Boolean
-    });
-  }
-
   initializing(scriptName, that) {
     this.values = utils.createValuesMap(this);
-    const TARGET_DIR = utils.getDir(this, scriptName);
-    const SOURCE_DIR = this.templatePath('../../../snippets/');
-    const PREFIX = utils.getPrefix();
-    const templates = this.values.templates;
-    const snippets = this.values.snippets;
-
-    //create the not-existent dir
-    if (!fs.existsSync(TARGET_DIR)) {
-      fs.mkdirSync(TARGET_DIR);
-    }
-
-    snippets.forEach((snippet, snippetIndex) => {
-      let filename = '';
-      if (hiddenSnippets.indexOf(snippet) >= 0) {
-        filename = PREFIX;
-      }
-      filename += snippet;
-
-      const templateIndex = templates.indexOf(snippet);
-      if (templateIndex >= 0) {
-        templates[templateIndex] = filename;
-      }
-      snippets[snippetIndex] = filename;
-      const data = fs.readFileSync(SOURCE_DIR + snippet);
-      fs.writeFileSync(TARGET_DIR + filename, data);
-    });
 
     if (that) {
       that.values = this.values;
